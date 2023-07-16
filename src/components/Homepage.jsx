d27da9f5b54c7024d9b55b3e895e18659ddb85a6
aravind <aravindtirumalasetty@gmail.com>
2023-07-16 12:53:48 +0530

    first commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "../components/Homepage.module.css";
+const Homepage = () => {
+  return (
+    <>
+    <div className={`${styles.newscontainer}`}>
+      webnews
+    </div>
+    </>
+  )
+}
+
+export default Homepage
