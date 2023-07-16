3c7adf8db5f09a6f0bd06feaf16aa4edfe9bf399
aravind <aravindtirumalasetty@gmail.com>
2023-07-16 14:56:38 +0530

    made hompage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import styles from "../components/Homepage.module.css";
+import axios from "axios";
 const Homepage = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=680fdb2b7b284742ae6299b7054b7db3"
+      )
+      .then((res) => {
+        console.log(res);
+        setNews(res.data.articles);
+      });
+  }, []);
   return (
     <>
-    <div className={`${styles.newscontainer}`}>
-     Hi Aravind! breifing your news
-    </div>
+      <div className={`${styles.newscontainer}`}>
+        <div className={`${styles.display}`}>
+          {news.map((newstopic, index) => {
+            const { title, url, urlToImage ,description} = newstopic;
+            return (
+              <>
+                <article key={index} className={`${styles.eacharticle}`}>
+                  <div className={`${styles.picture}`}>
+                  {urlToImage ? (
+                  <img
+                    src={urlToImage}
+                    alt="Dynamic"
+                    
+                  />
+                ) : (
+                  <img
+                    src="/images/noimage.png"
+                    alt="Default"
+                   
+                  />
+                )}
+                  </div>
+                  <div className={`${styles.details}`}>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <a href={url} className={`${styles.view}`} target="_blank" rel="noreferrer">
+                      View More
+                    </a>
+                  </div>
+                </article>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
